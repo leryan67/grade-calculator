028dec9e38be5eced9393e9ebffda69a81204a34
Ryan Le <leryan027@gmail.com>
2026-06-06 15:24:48 -0700

    Added letter grades determined through conditional statements

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class GradeCalculator {
     public static void main(String[] args) {
+        String letterGrade = "";
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a grade: ");
@@ -8,6 +9,25 @@ public class GradeCalculator {
 
         System.out.println("You entered: " + grade);
 
+        if (grade >= 90 && grade <= 100) {
+            letterGrade = "A";
+        }
+        else if (grade >= 80 && grade < 90) {
+            letterGrade = "B";
+        }
+        else if (grade >= 70 && grade < 80) {
+            letterGrade = "C";
+        }
+        else if (grade >= 60 && grade < 70) {
+            letterGrade = "D";
+        }
+        else if (grade >= 50 && grade < 60) {
+            letterGrade = "F";
+        }
+        else {
+            letterGrade = "\"You suck bro.\"";
+        }
+        
         input.close();
     }
 }
