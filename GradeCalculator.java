b85219f8cf5938bc19d40e97e9692772a1687aaa
Ryan Le <leryan027@gmail.com>
2026-06-06 15:13:23 -0700

    Added grade input

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class GradeCalculator {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a grade: ");
+        double grade = input.nextDouble();
+
+        System.out.println("You entered: " + grade);
+
+        input.close();
     }
 }
